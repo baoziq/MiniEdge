465625e201481688bd91997116e7103268e10a7e
baozi <caz69012@gmail.com>
2026-07-29 21:14:43 +0800

    feat: handle client read events with epoll

--- src/main.cc
+++ src/main.cc
@@ -8,12 +8,14 @@
 #include <iostream>
 #include <cstddef>
 #include <optional>
-#include <regex>
 #include <sys/epoll.h>
 #include <system_error>
 #include <unistd.h>
+#include <unordered_map>
 
 int main() {
+    std::unordered_map<int, std::optional<UniqueFd>> connection;
+
     Listener listener = Listener::create(8001);
     if (set_nonblocking(listener.fd()) < 0) {
         std::cerr << "set_nonblocking failed: " << std::strerror(errno) << std::endl;
@@ -34,7 +36,6 @@ int main() {
         return 1;
     }
     epoll_event events[16];
-    
     while (true) {
         int ready = epoll_wait(epoller.get(), &events[0], 16, -1);
         if (ready < 0) {
@@ -51,23 +52,59 @@ int main() {
                 // new connection coming
                 std::cout << "new connection comming\n";
                 std::optional<UniqueFd> client = listener.accept_connection();
-                set_nonblocking(client->get());
-                epoll_ctl(epoller.get(), EPOLL_CTL_ADD, client->get(), &event);
+                std::cout << "after accept\n";
                 if (!client.has_value()) {
                     std::cout << "No pending connection now\n";
                     return 0;
                 }
+                set_nonblocking(client->get());
+                epoll_event client_event{};
+                client_event.events = EPOLLIN;
+                client_event.data.fd = client->get();
+                if (epoll_ctl(epoller.get(), EPOLL_CTL_ADD, client->get(), &client_event) < 0) {
+                    throw std::system_error {
+                        errno,
+                        std::generic_category(),
+                        "epoll_ctl_add client_fd"
+                    };
+                }
+                connection[client->get()] = std::move(client);
             } else {
+                if (connection.find(fd) == connection.end()) {
+                    continue;
+                }
+                std::cout << "starting reading\n";
                 char buffer[1024];
                 ssize_t n = read(fd, buffer, sizeof(buffer));
                 if (n > 0 ){
                     write(STDOUT_FILENO, buffer, n);
+                } else if (n == 0) {
+                    std::cout << "connection has closed\n";
+                    if (epoll_ctl(epoller.get(), EPOLL_CTL_DEL, fd, nullptr) < 0) {
+                        throw std::system_error {
+                            errno,
+                            std::generic_category(),
+                            "epoll_ctl_del"
+                        };
+                        return 1;
+                    }
+                    connection.erase(fd);
+                } else {
+                    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                        continue;
+                    } else {
+                        throw std::system_error {
+                            errno,
+                            std::generic_category(),
+                            "read"
+                        };
+                        return 1;
+                    }
                 }
             }
         }
     }
     
-
     return 0;
 
 }
