b7412d3acec7e198b7ff1c791f932d2748314d87
baozi <caz69012@gmail.com>
2026-08-06 23:35:07 +0800

    remove tmp_send

--- src/http_parser.cc
+++ src/http_parser.cc
@@ -1,16 +1,15 @@
 #include "http_parser.h"
-#include "connection.h"
 
 HeaderParseResult parse_header(std::string_view input) {
     const auto index = input.find("\r\n\r\n");
     if (index == std::string::npos) {
-        if (input.size() > KMaxInputSize) {
+        if (input.size() > kMaxHeaderSize) {
             return {HeaderParseStatus::KTooLarge, 0};
         }
         return {HeaderParseStatus::KIncomplete, 0};
     }
     const std::size_t length = index + 4;
-    if (length > KMaxInputSize) {
+    if (length > kMaxHeaderSize) {
         return {HeaderParseStatus::KTooLarge, 0};
     }
     return {HeaderParseStatus::KComplete, length};
