b0da7fe8920ed3d769c7ea161802d9eff6849855
baozi <caz69012@gmail.com>
2026-08-10 22:19:08 +0800

    test: add header_fields_test

--- tests/http_parser_test.cc
+++ tests/http_parser_test.cc
@@ -46,9 +46,61 @@ void test_missing_version() {
     expect(result == LineParseStatus::KBadRequest, "miss version");
 }
 
+void test_complete_header() {
+    const std::string_view input = "GET /index.html HTTP/1.1\r\n"
+                                "Host: localhost\r\n"
+                                "Connection: close\r\n"
+                                "User-Agent: curl/8.0\r\n"
+                                "\r\n";
+    HttpRequest request;
+    const auto res = parse_header_fields(input, request);
+    expect(request.host == "localhost", "test_complete_headerhost");
+    expect(request.connection == "close", "test_complete_headerconnection");
+    expect(res == HeaderFieldsParseStatus::KClose, "test_complete_headerstatus");
+    
+}
+
+void test_lower_header() {
+    const std::string_view input = "GET /index.html HTTP/1.1\r\n"
+                                "host: localhost\r\n"
+                                "connection: close\r\n"
+                                "User-Agent: curl/8.0\r\n"
+                                "\r\n";
+    HttpRequest request;
+    const auto res = parse_header_fields(input, request);
+    expect(res == HeaderFieldsParseStatus::KClose, "test_lower_headerstatus");
+    expect(request.host == "localhost", "test_lower_headerhost");
+    expect(request.connection == "close", "test_lower_headerconnection");
+}
+
+void test_bad_request_header() {
+    const std::string_view input = "GET /index.html HTTP/1.1\r\n"
+                                "connection: close\r\n"
+                                "User-Agent: curl/8.0\r\n"
+                                "\r\n";
+    HttpRequest request;
+    const auto res = parse_header_fields(input, request);
+    expect(res == HeaderFieldsParseStatus::KBadRequest, "status");
+    expect(request.host == "", "host");
+}
+
+void test_nonclose_header() {
+    const std::string_view input = "GET /index.html HTTP/1.1\r\n"
+                                "Host: localhost\r\n"
+                                "User-Agent: curl/8.0\r\n"
+                                "\r\n";
+    HttpRequest request;
+    const auto res = parse_header_fields(input, request);
+    expect(res == HeaderFieldsParseStatus::KComplete, "test_bad_request_headerstatus");
+}
+
 int main() {
     test_complete_line();
     test_missing_target();
     test_missing_version();
+    test_complete_header();
+    test_lower_header();
+    test_bad_request_header();
+    test_nonclose_header();
     std::cout << "All HTTP parser tests passed.\n";
 }
