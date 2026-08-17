1a21d0f9ae506fdf46ab8937c00bb955b9cbe780
baozi <caz69012@gmail.com>
2026-08-17 22:16:34 +0800

    fix: bing_upstream find_by_client

--- src/session_registry.cc
+++ src/session_registry.cc
@@ -10,9 +10,10 @@ bool SessionRegistry::create(int client_fd) {
 
 bool SessionRegistry::bind_upstream(int client_fd, UniqueFd upstream_fd) {
     auto it = sessions_.find(client_fd);
-    if (it == sessions_.end()) {
+    if (it == sessions_.end() || it->second.upstream_fd.has_value()) {
         return false;
     }
+    
     it->second.upstream_fd = std::move(upstream_fd);
     auto [it1, inserted] = upstream_to_client_.try_emplace(it->second.upstream_fd->get(), client_fd);
     return inserted;
@@ -21,9 +22,6 @@ bool SessionRegistry::bind_upstream(int client_fd, UniqueFd upstream_fd) {
 ProxySession* SessionRegistry::find_by_client(int client_fd) {
     auto it = sessions_.find(client_fd);
     if (it == sessions_.end()) {
-        return nullptr;
-    }
-    if (!it->second.upstream_fd.has_value()) {
         return nullptr;
     }
     return &it->second;
@@ -50,12 +48,10 @@ bool SessionRegistry::erase_by_client(int client_fd) {
     if (!it->second.upstream_fd.has_value()) {
         return false;
     }
-    int upstream_fd = it->second.upstream_fd->get();
-    sessions_.erase(client_fd);
-    auto it1 = upstream_to_client_.find(upstream_fd);
-    if (it1 == upstream_to_client_.end()) {
-        return false;
+    if (it->second.upstream_fd.has_value()) {
+        int upstream_fd = it->second.upstream_fd->get();
+        upstream_to_client_.erase(upstream_fd);
     }
-    upstream_to_client_.erase(upstream_fd);
+    sessions_.erase(it);
     return true;
 }
