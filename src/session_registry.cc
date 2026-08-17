ec2ddca1d823a90a6c4d834bfaf8d795e43fdf92
baozi <caz69012@gmail.com>
2026-08-17 21:53:15 +0800

    feat: add proxy session registry

---
+++ src/session_registry.cc
@@ -0,0 +1,61 @@
+#include "session_registry.h"
+#include "unique_fd.h"
+#include <optional>
+
+bool SessionRegistry::create(int client_fd) {
+    ProxySession session{client_fd, std::nullopt};
+    auto [it, inserted] = sessions_.try_emplace(client_fd, std::move(session));
+    return inserted;
+}
+
+bool SessionRegistry::bind_upstream(int client_fd, UniqueFd upstream_fd) {
+    auto it = sessions_.find(client_fd);
+    if (it == sessions_.end()) {
+        return false;
+    }
+    it->second.upstream_fd = std::move(upstream_fd);
+    auto [it1, inserted] = upstream_to_client_.try_emplace(it->second.upstream_fd->get(), client_fd);
+    return inserted;
+}
+
+ProxySession* SessionRegistry::find_by_client(int client_fd) {
+    auto it = sessions_.find(client_fd);
+    if (it == sessions_.end()) {
+        return nullptr;
+    }
+    if (!it->second.upstream_fd.has_value()) {
+        return nullptr;
+    }
+    return &it->second;
+}
+
+ProxySession* SessionRegistry::find_by_upstream(int upstream_fd) {
+    auto it = upstream_to_client_.find(upstream_fd);
+    if (it == upstream_to_client_.end()) {
+        return nullptr;
+    }
+    int client_fd = it->second;
+    auto it1 = sessions_.find(client_fd);
+    if (it1 == sessions_.end()) {
+        return nullptr;
+    }
+    return &it1->second;
+}
+
+bool SessionRegistry::erase_by_client(int client_fd) {
+    auto it = sessions_.find(client_fd);
+    if (it == sessions_.end()) {
+        return false;
+    }
+    if (!it->second.upstream_fd.has_value()) {
+        return false;
+    }
+    int upstream_fd = it->second.upstream_fd->get();
+    sessions_.erase(client_fd);
+    auto it1 = upstream_to_client_.find(upstream_fd);
+    if (it1 == upstream_to_client_.end()) {
+        return false;
+    }
+    upstream_to_client_.erase(upstream_fd);
+    return true;
+}
