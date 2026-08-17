6abd789a36f844b42784c9f60ee182f7ddc37d9d
baozi <caz69012@gmail.com>
2026-08-17 21:53:33 +0800

    feat: add proxy session registry

---
+++ include/session_registry.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "unique_fd.h"
+
+#include <optional>
+#include <unordered_map>
+
+enum class ProxyState{
+    KReadingRequest,
+    KConnectingUpstream,
+    KSendingRequest,
+    KReadingResponse,
+    KSendingResponse,
+    KClosing
+};
+
+struct ProxySession{
+    int client_fd = -1;
+    std::optional<UniqueFd> upstream_fd;
+    ProxyState state{ProxyState::KReadingRequest};
+};
+
+class SessionRegistry {
+public:
+    bool create(int client_fd);
+    bool bind_upstream(int client_fd, UniqueFd upstream_fd);
+
+    ProxySession* find_by_client(int client_fd);
+    ProxySession* find_by_upstream(int upstream_fd);
+
+    bool erase_by_client(int client_fd);
+private:
+    std::unordered_map<int, ProxySession> sessions_;
+    std::unordered_map<int, int> upstream_to_client_;
+};
